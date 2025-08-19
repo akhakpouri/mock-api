9c31dd327df2230d5dcbc6e4634024aa03ac6b7e
Ali Khakpouri <ali.khakpouri@gmail.com>
2025-08-19 11:58:22 -0400

    finished the test methods

--- model/wallet.go
+++ model/wallet.go
@@ -1,13 +1,28 @@
 package model
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+type Bitcoin int
+type Stringer interface {
+	String() string
 }
 
-func (w Wallet) Balance() int {
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
