9a69ac393e752aa206222d3a733a9275a4f23913
Ali Khakpouri <ali.khakpouri@gmail.com>
2025-08-27 11:43:59 -0400

    added the map test

---
+++ model/map_test.go
@@ -0,0 +1,21 @@
+package model
+
+import "testing"
+
+var colors = map[string]string{
+	"black": "#000",
+	"white": "#fff",
+}
+
+func TestMap(t *testing.T) {
+	got := search("black")
+	want := "#000"
+
+	if got != want {
+		t.Errorf("got %q, but really wanted %q", got, want)
+	}
+}
+
+func search(word string) string {
+	return colors[word]
+}
