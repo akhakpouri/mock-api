b046c0bca0a611b31f32ca5ae0cf062efd564835
Ali Khakpouri <ali.khakpouri@gmail.com>
2025-08-28 11:52:18 -0400

    updated the dic

--- model/dictionary.go
+++ model/dictionary.go
@@ -1,7 +1,17 @@
 package model
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(key string) (string, error) {
+	define, ok := d[key]
+	if !ok {
+		return "", errors.New("didn't find the color you were looking for")
+	}
+	return define, nil
+}
+
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
 }
--- model/map_test.go
+++ model/map_test.go
@@ -1,17 +1,48 @@
 package model
 
-import "testing"
+import (
+	"testing"
+)
 
 var colors = Dictionary{
 	"black": "#000",
 	"white": "#fff",
 }
 
-func TestMap(t *testing.T) {
-	got := colors.Search("black")
-	want := "#000"
+func printMapError(got string, want string, t testing.TB) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q, but really wanted %q", got, want)
+		t.Errorf("failed. wanted %q, but got %q", want, got)
+	} else {
+		t.Logf("success. wanted %q, and got %q", want, got)
 	}
+}
+
+func TestMap(t *testing.T) {
+	t.Run("Known color", func(t *testing.T) {
+		got, _ := colors.Search("white")
+		want := "#fff"
+		printMapError(got, want, t)
+	})
+	t.Run("Unknown color", func(t *testing.T) {
+		got, err := colors.Search("purple")
+		want := "#000"
+		if err == nil {
+			t.Fatal("Excepted an error, but didn't get one")
+		}
+		printMapError(got, want, t)
+	})
+	t.Run("Add color", func(t *testing.T) {
+		key, value := "gray", "#999"
+		colors.Add(key, value)
+
+		want := value
+		got, err := colors.Search(key)
+
+		if err != nil {
+			t.Fatalf("color %q wasn't added correctly.", key)
+		}
+		printMapError(got, want, t)
+	})
 }
