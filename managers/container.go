75330ebc096339058c48b1af388d6284b57756b6
Ali Khakpouri <ali.khakpouri@gmail.com>
2025-09-10 10:16:32 -0400

    moved the method to the correct manager

--- managers/container.go
+++ managers/container.go
@@ -2,9 +2,6 @@ package managers
 
 import (
 	"log"
-	"math/rand"
-	"mock-api/model"
-	"time"
 
 	"go.uber.org/dig"
 )
@@ -23,14 +20,4 @@ func GetDig() {
 	if err := container.Invoke(SayHi); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func newAccount(name string) model.Account {
-	id := rand.Intn(100)
-	year := 1984
-	day := 1
-	month := time.November
-	birthDate := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
-	return model.Account{Id: id, Name: name, BirthDate: birthDate}
-
 }
