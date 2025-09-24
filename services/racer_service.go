ec7851c6e98de6a325980d8c7e44e0c7f69b18b3
Ali Khakpouri <ali.khakpouri@gmail.com>
2025-09-24 16:18:28 -0400

    completed the select and created a mock racer

--- services/racer_service.go
+++ services/racer_service.go
@@ -1,22 +1,32 @@
 package services
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Race(first, second string) (winner string) {
-	firstResult := duration(first)
-	secondResult := duration(second)
-
-	if firstResult >= secondResult {
-		return first
-	}
-	return second
+func Race(first, second string) (winner string, err error) {
+	timeout := (10 * time.Second)
+	return configureRacer(first, second, timeout)
 }
 
-func duration(url string) int64 {
-	start := time.Now()
-	http.Get(url)
-	return int64(time.Since(start).Seconds())
+func configureRacer(first, second string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(first):
+		return first, nil
+	case <-ping(second):
+		return second, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("time out while waiting for %s and %s", first, second)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
--- services/racer_test.go
+++ services/racer_test.go
@@ -8,23 +8,41 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	first := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
+	t.Run("compare the two servers", func(t *testing.T) {
+		first := createServer(20 * time.Millisecond)
+		second := createServer(5 * time.Millisecond)
+
+		defer first.Close()
+		defer second.Close()
+
+		got, _ := Race(first.URL, second.URL)
+		want := second.URL
+
+		if got != want {
+			t.Errorf("wanted %q, but ended up getting %q", want, got)
+		}
+	})
+
+	t.Run("exception occurs if response is longer than 10s", func(t *testing.T) {
+		first := createServer(20 * time.Millisecond)
+		second := createServer(5 * time.Millisecond)
+
+		defer first.Close()
+		defer second.Close()
+
+		_, err := Race(first.URL, second.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one.")
+		}
+
+	})
+
+}
+
+func createServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
-
-	second := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(5 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	got := Race(first.URL, second.URL)
-	want := first.URL
-
-	if got != want {
-		t.Errorf("wanted %q, but ended up getting %q", want, got)
-	}
-
-	first.Close()
-	second.Close()
 }
